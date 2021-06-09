8c470bf19dcc10c9522ad6dca4c4895c58955b73
SamanthaDi <minuediaz31@outlook.com>
2021-06-09 11:53:52 -0500

    "Exercise 6"

--- src/test/java/uaslp/objetos/escuela/Exercise6.java
+++ src/test/java/uaslp/objetos/escuela/Exercise6.java
@@ -1,8 +1,11 @@
 package uaslp.objetos.escuela;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.*;
 
 public class Exercise6 {
 
@@ -24,5 +27,53 @@ public class Exercise6 {
         assertThat(Dependencia1.class).isInterface();
         assertThat(Dependencia2.class).isInterface();
         assertThat(Dependencia3.class).isInterface();
+    }
+
+    @Test
+    public void validarMetodoCubir_CuandoAyBSonIguales_entoncesGuardamosCadenaC(){
+        Dependencia1 dependencia1 = Mockito.mock(Dependencia1.class);
+        Dependencia2 dependencia2 = Mockito.mock(Dependencia2.class);
+        Dependencia3 dependencia3 = Mockito.mock(Dependencia3.class);
+        AlgoritmoX algoritmo = new AlgoritmoX(dependencia1,dependencia2,dependencia3);
+
+        //when
+        int var = 5;
+        algoritmo.algoritmoACubrir(var,var,"cadena C");
+
+        //then
+        verify(dependencia1, times(1)).save("cadena C");
+
+    }
+
+    @Test
+    public void validarMetodoCubir_CuandoAEsMenorQueB_entoncesLlamammosAMetodoPrint(){
+        Dependencia1 dependencia1 = Mockito.mock(Dependencia1.class);
+        Dependencia2 dependencia2 = Mockito.mock(Dependencia2.class);
+        Dependencia3 dependencia3 = Mockito.mock(Dependencia3.class);
+        AlgoritmoX algoritmo = new AlgoritmoX(dependencia1,dependencia2,dependencia3);
+
+        //when
+
+        algoritmo.algoritmoACubrir(3, 5,"cadena C");
+
+        //then
+        verify(dependencia2, times(1)).print(3, "cadena C");
+
+    }
+
+    @Test
+    public void validarMetodoCubir_CuandoAEsMayorQueB_entoncesLlamamosMetodoSendYVerificamosMetodoRecover(){
+        Dependencia1 dependencia1 = Mockito.mock(Dependencia1.class);
+        Dependencia2 dependencia2 = Mockito.mock(Dependencia2.class);
+        Dependencia3 dependencia3 = Mockito.mock(Dependencia3.class);
+        AlgoritmoX algoritmo = new AlgoritmoX(dependencia1,dependencia2,dependencia3);
+
+        //when
+        algoritmo.algoritmoACubrir(7,4,"cadena C");
+
+        //then
+        verify(dependencia3, times(1)).send(7, 4);
+        verify(dependencia3, times(1)).recover();
+
     }
 }
